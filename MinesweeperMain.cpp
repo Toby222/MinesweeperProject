258c6de14fb16bc5cb74c09e9c9434f182997607
toman222 <tobi.berger13@gmail.com>
2020-03-17 14:17:48 +0100

    generates random field at this point TryOpen is very impermanent for some reason

--- MinesweeperMain.cpp
+++ MinesweeperMain.cpp
@@ -1,51 +1,56 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 #include <vector>
-#include <array>
+#include <random>
 
 #define MS_FIELD_SIZE 16
 #define MS_TOPBAR_SIZE 32
 
 namespace minesweeper {
 	struct Sprites {
-		Sprites() {
-			std::cout << "Loading sprites...\n";
-			for (int i = 0; i < 9; i++) {
-				std::stringstream filename;
-				filename << "Sprites/field" << i << ".png";
-				try
-				{
-					std::cout << filename.str() << std::endl;
-					auto newSprite = olc::Sprite(filename.str());
+		olc::Sprite* LoadSprite(std::string filename) {
+			std::cout << filename << std::endl;
+			std::stringstream filepath;
+			filepath << "Sprites/" << filename;
 
-					if (newSprite.width == 0 || newSprite.height == 0)
-						throw std::exception("Invalid file.");
+			auto newSprite = new olc::Sprite(filepath.str());
 
-					if (newSprite.width != MS_FIELD_SIZE || newSprite.height != MS_FIELD_SIZE) {
-						std::stringstream exceptionString;
-						exceptionString << "Loaded sprite is invalid size. Needs to be " << MS_FIELD_SIZE << "x" << MS_FIELD_SIZE << std::endl;
-						throw std::exception(exceptionString.str().c_str());
-					}
+			if (newSprite->width == 0 || newSprite->height == 0)
+				throw std::exception("Invalid file.");
 
-					fieldSprites[i] = newSprite;
-				}
-				catch (const std::exception & err)
-				{
-					std::cerr << "Terminating during loading process:\n  " << err.what() << std::endl;
-					std::cin.get();
-					exit(EXIT_FAILURE);
-				}
+			if (newSprite->width != MS_FIELD_SIZE || newSprite->height != MS_FIELD_SIZE) {
+				std::stringstream exceptionString;
+				exceptionString << "Loaded sprite is invalid size. Needs to be " << MS_FIELD_SIZE << "x" << MS_FIELD_SIZE << std::endl;
+				throw std::exception(exceptionString.str().c_str());
 			}
-			std::cout << "Sprites/closed.png" << std::endl;
-			closed = olc::Sprite("Sprites/closed.png");
-			std::cout << "Sprites/flagged.png" << std::endl;
-			flagged = olc::Sprite("Sprites/flagged.png");
+
+			return newSprite;
 		}
 
-		olc::Sprite fieldSprites[9];
-		olc::Sprite closed;
-		olc::Sprite flagged;
+		Sprites() {
+			std::cout << "Loading sprites...\n";
+			try
+			{
+				for (int i = 0; i < 9; i++) {
+					std::stringstream filename;
+					filename << "field" << i << ".png";
+					fieldSprites[i] = LoadSprite(filename.str());
+				}
+				flagged = LoadSprite("flagged.png");
+				closed = LoadSprite("closed.png");
+				revealedmine = LoadSprite("revealedmine.png");
+			}
+			catch (const std::exception & err)
+			{
+				std::cerr << "Terminating during loading process:\n  " << err.what() << std::endl;
+				std::cin.get();
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		static olc::Sprite* fieldSprites[9], * closed, * flagged, * revealedmine;
 	};
+	olc::Sprite* Sprites::fieldSprites[9], * Sprites::closed, * Sprites::flagged, * Sprites::revealedmine;
 
 	class Minesweeper : public olc::PixelGameEngine {
 	private:
@@ -58,31 +63,46 @@ namespace minesweeper {
 			};
 
 		public:
-			Square(bool mine, olc::vi2d position) : position(position)
+			Square(bool isMine, olc::vi2d position) : position(position)
 			{
-				this->mine = mine;
+				this->isMine = isMine;
 			}
 
 			const olc::vi2d position;
 
-			bool mine;
+			bool isMine;
 			int value = 0;
 			bool flagged = false;
 			State state = State::closed;
 
-			olc::Sprite getSprite() {
-				return olc::Sprite("Sprites/closed.png");
-				/*switch (state) {
+			olc::Sprite* getSprite() {
+				if (isMine)
+					return Sprites::revealedmine;
+				switch (state) {
 					case(State::closed):
-						return sprites.closed;
+						return Sprites::closed;
 					case(State::flagged):
-						return sprites.flagged;
+						return Sprites::flagged;
 					case(State::open):
-						return sprites.fieldSprites[value];
+						return Sprites::fieldSprites[value];
 					default:
 						throw std::exception("Invalid Square state");
-				}*/
+				}
 			};
+
+			void TryOpen() {
+				switch (this->state)
+				{
+					case State::closed:
+						this->state = State::open;
+						break;
+					case State::open:
+					case State::flagged:
+						break;
+					default:
+						break;
+				}
+			}
 		};
 
 	public:
@@ -90,18 +110,60 @@ namespace minesweeper {
 
 		Minesweeper()
 		{
-			for (int y = 0; y < 16; y++) {
-				field.push_back(std::vector<Square>());
-				for (int x = 0; x < 16; x++)
-					field[y].push_back(Square(false, olc::vi2d(x, y)));
-			}
-
 			sAppName = "Minesweeper";
 		}
 
 		// Called once at the start, so create things here
 		bool OnUserCreate() override
 		{
+			Sprites::Sprites();
+			std::uniform_real<> random(0, 1);
+			std::default_random_engine generator;
+
+			if ((MS_FIELD_SIZE & MS_FIELD_SIZE >> 1) != 0)
+				throw std::exception("Invalid field size. Has to be power of 2.");
+
+			if (ScreenWidth() % MS_FIELD_SIZE != 0)
+				throw std::exception("Invalid screen width.");
+			if (ScreenHeight() % MS_FIELD_SIZE != 0)
+				throw std::exception("Invalid screen height.");
+
+			for (int y = 0; y < (ScreenHeight() - MS_TOPBAR_SIZE) / MS_FIELD_SIZE; y++) {
+				field.push_back(std::vector<Square>());
+				for (int x = 0; x < ScreenWidth() / MS_FIELD_SIZE; x++) {
+					field[y].push_back(Square(random(generator) <= 0.25, olc::vi2d(x, y)));
+				}
+			}
+
+			olc::vi2d offsets[8]{
+				olc::vi2d(-1, 1),
+				olc::vi2d(0, 1),
+				olc::vi2d(1, 1),
+				olc::vi2d(-1, 0),
+				olc::vi2d(1,0),
+				olc::vi2d(-1,1),
+				olc::vi2d(0, 1),
+				olc::vi2d(1, 1)
+			};
+
+			for (auto row : field)
+			{
+				for (auto square : row)
+				{
+					for (auto offset : offsets) {
+						auto neighborPos = square.position + offset;
+						if (neighborPos.y >= field.size() ||
+							neighborPos.y < 0 ||
+							neighborPos.x >= field[0].size() ||
+							neighborPos.x < 0)
+							continue;
+						// std::cout << "X: " << neighborPos.x << " Y: " << neighborPos.y << std::endl;
+						if (field[neighborPos.y][neighborPos.x].isMine)
+							square.value++;
+					}
+					(&square)->TryOpen();
+				}
+			}
 			return true;
 		}
 
@@ -117,7 +179,7 @@ namespace minesweeper {
 
 			for (auto row : field)
 				for (auto square : row)
-					DrawSprite(square.position.x*MS_FIELD_SIZE, square.position.y*MS_FIELD_SIZE+MS_TOPBAR_SIZE, &square.getSprite());
+					DrawSprite(square.position.x * MS_FIELD_SIZE, square.position.y * MS_FIELD_SIZE + MS_TOPBAR_SIZE, square.getSprite());
 
 			return true;
 		}
@@ -127,7 +189,7 @@ namespace minesweeper {
 int main()
 {
 	minesweeper::Minesweeper mainWindow;
-	if (mainWindow.Construct(256, 256 + MS_TOPBAR_SIZE, 2, 2))
+	if (mainWindow.Construct(1024, 512 + MS_TOPBAR_SIZE, 1, 1))
 		mainWindow.Start();
 
 	return 0;
